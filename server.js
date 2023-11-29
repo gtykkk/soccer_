b5c007e4c88bbc421c25893091b3948c4a477645
gtykkk <qwez0498@gmail.com>
2023-11-29 18:07:29 +0900

    상단 리그 클릭 시 css 변경

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const ejs = require('ejs');
 const app = express();
-const { teams, epl, primera, bundesliga, seria, league1 } = require('./apikey.js');
+const { teams, epl, primera, bundesliga, seria, league1, logo } = require('./apikey.js');
 
 app.use(express.json())
 app.use(express.static(__dirname + '/css'));
@@ -36,6 +36,7 @@ app.get('/rank', async (요청, 응답) => {
     const BUNDESLIGA_RANK = bundesliga;
     const SERIA_RANK = seria;
     const LEAGUE1_RANK = league1;
+    const LOGO = logo;
 
     try {
         const epl_response = await axios.request(EPL_RANK);
@@ -43,15 +44,21 @@ app.get('/rank', async (요청, 응답) => {
         const bundesliga_response = await axios.request(BUNDESLIGA_RANK);
         const seria_response = await axios.request(SERIA_RANK);
         const league1_response = await axios.request(LEAGUE1_RANK);
+        const logo = await axios.request(LOGO);
 
         응답.render('rank.ejs', { 
             epl_rank : epl_response.data.response[0].league,
             primera_rank : primera_response.data.response[0].league,
             bundesliga_rank : bundesliga_response.data.response[0].league,
             seria_rank : seria_response.data.response[0].league,
-            league1_rank : league1_response.data.response[0].league
+            league1_rank : league1_response.data.response[0].league,
+            logo : logo.data
         });
     } catch (error) {
         console.error("rank : ", error);
     }
+});
+
+app.get('/copy', async (요청, 응답) => {
+    응답.render('rank_copy.ejs');
 });
